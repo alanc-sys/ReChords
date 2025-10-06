dbed0052da26bd92d85d3056ecb07c0a06161cd2
macbook <cralan735@gmail.com>
2025-10-06 13:13:35 -0300

    Refactorizacion

---
+++ src/main/java/com/misacordes/application/dto/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.misacordes.application.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    String username;
+    String password;
+}
